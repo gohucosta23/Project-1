1ca81acb064c596c4fc2c93c1f4b26a65c6ebbcf
Hugo_Costa <gohucosta23@hotmail.com>
2020-03-27 22:53:47 -0400

    Deleting unused files

--- home.js
+++ home.js
@@ -5,28 +5,19 @@ $(document).ready(function(){
         $(document).ready(function () {
             var recipeBtn = $("#buttons1");
             var drinkBtn = $("#buttons2");
-
             recipeBtn.on("click", function () {
                 window.location = "recipes.html";
             })
-
             drinkBtn.on("click", function () {
-                window.location = "index-copy-2.html";
+                window.location = "cocktail.html";
             })
-            
-
             function blink() {
-
-
                 if ($("#sign").css("border-color") === "rgb(255, 255, 255)") {
-
                     $("#sign").css("border-color", "yellow");
                 }
                 else {
-
                     $("#sign").css("border-color", "rgb(255, 255, 255)");
                 }
-
             }
             setInterval(blink, 500);
         });
